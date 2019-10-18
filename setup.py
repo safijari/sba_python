9de13acd251a3b386caf504fe75ca6bd274e5940
Jariullah Safi <safijari@isu.edu>
2019-10-18 15:34:28 -0700

    Builds correctly now I think

--- setup.py
+++ setup.py
@@ -1,17 +1,93 @@
 from setuptools import setup, find_packages
-try:
-    import pybind11_cmake
-except ImportError:
-    print("pybind11-cmake must be installed."
-          "Try \n \t pip install pybind11_cmake")
-    import sys
-    sys.exit()
 
-from pybind11_cmake import CMakeExtension, CMakeBuild
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+is_ros1 = False
+
+try:
+    import rospy
+    is_ros1 = True
+except Exception:
+    pass
+
+is_ros2 = False
+
+try:
+    import rclpy
+    is_ros2 = True
+except Exception:
+    pass
+
+assert is_ros2 == False, "Not currently set up to work with ros2"
+
+if not is_ros1 and not is_ros2:
+    try:
+        import pybind11_cmake
+    except ImportError:
+        print("pybind11-cmake must be installed."
+            "Try \n \t pip install pybind11_cmake")
+        import sys
+        sys.exit()
 
 setup(
     name='sparse_bundle_adjustment',
-    version='0.9.1',
+    version='0.4.3',
     author='',
     author_email='',
     description='',

cf7663d711b53d9a4f24adf8d459b5e9c518241b
Jariullah <safijari@isu.edu>
2024-01-18 19:24:26 -0600

    _actually_ builds

--- docker_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker run -v $DIR:/repo --rm safijari/manylinux_2_28-x64 /repo/build.sh
--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-sudo docker build --no-cache -t sba_builder .
+#sudo docker build --no-cache -t sba_builder .
 cd /tmp
 wget https://gitlab.com/libeigen/eigen/-/archive/3.3.3/eigen-3.3.3.tar.gz
+sudo tar -xzf eigen-3.3.3.tar.gz
+cd -
 sudo docker run -v $DIR:/repo -v /tmp/eigen-3.3.3:/eigenfolder --rm sba_builder /repo/build.sh
